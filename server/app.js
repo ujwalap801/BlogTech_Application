6ed38c4839c14c65e56a3562c47f872b718fb066
Ujwala <ujwalap801@gmail.com>
2025-06-30 22:42:45 +0530

    Added filed

--- server/app.js
+++ server/app.js
@@ -32,7 +32,7 @@ app.use("/author-api",authorApp);
 
 // Database connection
 
-const port = process.env.PORT || 4000;
+const port = process.env.PORT;
 
 async function main() {
     await mongose.connect(process.env.DBURL);
